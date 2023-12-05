15096dd629075e2c077c34a05cfa77f713548642
daydreamer22119@gmail.com <daydreamer22119@gmail.com>
2023-12-05 22:36:43 +0530

    server commit

---
+++ ATM-SPRING-SERVER/src/main/java/com/dcodetech/ATMMACHINE/AtmSpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.dcodetech.ATMMACHINE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmSpringServerApplication.class, args);
+	}
+
+}
---
+++ ATM-SPRING-SERVER/src/test/java/com/dcodetech/ATMMACHINE/AtmSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcodetech.ATMMACHINE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmSpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
