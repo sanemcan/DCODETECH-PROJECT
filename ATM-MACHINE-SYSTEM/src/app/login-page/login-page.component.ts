76e36a024d80411dd135900764ef20243dac0b7e
Prachi-Ashtivkar <prachiashtivkar23@gmail.com>
2023-12-21 22:33:00 +0530

    Login,Register,Money Transfer Api + frontend all done

--- ATM-MACHINE-SYSTEM/src/app/login-page/login-page.component.ts
+++ ATM-MACHINE-SYSTEM/src/app/login-page/login-page.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent {
+export class LoginPageComponent  implements OnInit {
+   loginForm!: FormGroup;
+
+   constructor(private fb: FormBuilder,private userService:UserService) { }
+
+   ngOnInit(): void {
+     this.initializeForm();
+   }
+
+   get registerButtonColor(): string {
+    return this.loginForm.valid ? '#2d6a4f' : '#b7e4c7';
+  }
+
+  initializeForm(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      
+    });
+
+
 
 }
+
+
+
+}
+
